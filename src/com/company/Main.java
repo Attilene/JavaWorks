1412b1ea4378898f3b0467050cf6852c9dd6a69f
Артем <artembakanov123@yandex.ru>
2020-09-10 11:23:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 4;
+        double b = 0;
+        System.out.print(a / b);
+
+    }
+}
