6e093fa60e66843104eeea74b11302d608f0f52d
Артем <artembakanov123@yandex.ru>
2020-09-10 11:59:11 +0300

    .

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Seminar_2.*;
+import Seminar_1.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Seminar_2/Task1_Matrix.java
@@ -0,0 +1,13 @@
+package Seminar_2;
+
+public class Task1_Matrix {
+    double[][] matrix;
+
+    public Task1_Matrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public double[][] plus(double[][] matrix2) {
+        return matrix2;
+    }
+}
