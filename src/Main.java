3d276b34204ce2e3a9f7f67ef67a28e924d7a9a8
Артем <artembakanov123@yandex.ru>
2020-09-10 19:51:52 +0300

    Выполнил первую задачу

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 import Seminar_2.*;
 import Seminar_1.*;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        double[][] matrix1 = {
+                {1, 2, -1},
+                {3, 1, 2},
+                {0, 2, 4}
+        };
+        Task1_Matrix matrix = new Task1_Matrix(matrix1);
+        double[][] matrix2 = {
+                {3, 6, 10},
+                {-2, 4, 5},
+                {12, -4, 2}
+        };
+        Task1_Matrix.matrout(matrix.plus(matrix2));
+        Task1_Matrix.matrout(matrix.minus(matrix2));
+        Task1_Matrix.matrout(matrix.mulcount(5));
+        Task1_Matrix.matrout(matrix.mulmat(matrix2));
+        Task1_Matrix.matrout(matrix.transpon());
+        Task1_Matrix.matrout(matrix.stepen(3));
     }
 }
--- src/Seminar_2/Task1_Matrix.java
+++ src/Seminar_2/Task1_Matrix.java
@@ -2,12 +2,116 @@ package Seminar_2;
 
 public class Task1_Matrix {
     double[][] matrix;
+    int leni;
+    int lenj;
 
     public Task1_Matrix(double[][] matrix) {
         this.matrix = matrix;
+        this.leni = this.matrix.length;
+        this.lenj = this.matrix[0].length;
     }
 
     public double[][] plus(double[][] matrix2) {
-        return matrix2;
+        double[][] matrix3 = matr3();
+        for (int i = 0; i < matrix3.length; i++) {
+            for (int j = 0; j < matrix3[i].length; j++) {
+                matrix3[i][j] += matrix2[i][j];
+            }
+        }
+        return matrix3;
+    }
+
+    public double[][] minus(double[][] matrix2) {
+        double[][] matrix3 = matr3();
+        for (int i = 0; i < matrix3.length; i++) {
+            for (int j = 0; j < matrix3[i].length; j++) {
+                matrix3[i][j] -= matrix2[i][j];
+            }
+        }
+        return matrix3;
+    }
+
+    public double[][] mulcount(double c) {
+        double[][] matrix3 = matr3();
+        for (int i = 0; i < matrix3.length; i++) {
+            for (int j = 0; j < matrix3[i].length; j++) {
+                matrix3[i][j] *= c;
+            }
+        }
+        return matrix3;
+    }
+
+    public double[][] mulmat(double[][] matrix2) {
+        double sumel = 0;
+        double[] buf = new double[matrix2[0].length];
+        double[][] matrix3 = new double[leni][buf.length];
+        for (int i = 0; i < leni; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int k = 0; k < lenj; k++) {
+                    sumel = sumel + this.matrix[i][k] * matrix2[k][j];
+                }
+                buf[j] = sumel;
+                sumel = 0;
+            }
+            for (int q = 0; q < lenj; q++) {
+                matrix3[i][q] = buf[q];
+            }
+        }
+        return matrix3;
+    }
+
+    public double[][] transpon() {
+        double[][] matrix3 = new double[lenj][leni];
+        double[] buf = new double[lenj];
+        for (int j = 0; j < lenj; j++) {
+            for (int i = 0; i < leni; i++) {
+                buf[i] = this.matrix[i][j];
+            }
+            for (int i = 0; i < leni; i++) {
+                matrix3[j][i] = buf[i];
+            }
+        }
+        return matrix3;
+    }
+
+    public double[][] stepen(double st) {
+        double sumel = 0;
+        double[] buf = new double[lenj];
+        double[][] matrix3 = matr3();
+        for (int t = 0; t < st - 1; t++) {
+            for (int i = 0; i < leni; i++) {
+                for (int j = 0; j < lenj; j++) {
+                    for (int k = 0; k < lenj; k++) {
+                        sumel = sumel + matrix3[i][k] * this.matrix[k][j];
+                    }
+                    buf[j] = sumel;
+                    sumel = 0;
+                }
+                for (int q = 0; q < lenj; q++) {
+                    matrix3[i][q] = buf[q];
+                }
+            }
+        }
+        return matrix3;
+    }
+
+    public double[][] matr3() {
+        double[][] matrix3 = new double[leni][lenj];
+        for (int i = 0; i < leni; i++) {
+            for (int j = 0; j < lenj; j++) {
+                matrix3[i][j] = this.matrix[i][j];
+            }
+        }
+        return matrix3;
+    }
+
+    public static void matrout(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
