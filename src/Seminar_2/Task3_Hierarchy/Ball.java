83957ada06bc5d44c3c907a75a371c432102d639
Артем <artembakanov123@yandex.ru>
2020-09-12 19:38:04 +0300

    Выполнил третью задачу

--- src/Seminar_2/Task3_Hierarchy/Ball.java
+++ src/Seminar_2/Task3_Hierarchy/Ball.java
@@ -3,6 +3,6 @@ package Seminar_2.Task3_Hierarchy;
 public class Ball extends SolidOfRevolution{
     public Ball(double radius) {
         super(radius);
-        super.volume = (4 * Math.PI * Math.pow(super.radius, 3) / 3);
+        super.volume = (4 * Math.PI * Math.pow(super.radius, 3) / 3.0);
     }
 }
