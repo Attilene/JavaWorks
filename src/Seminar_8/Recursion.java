8ab0791682d723895c24f3397de801a65fddf12b
Артем <artembakanov123@yandex.ru>
2020-11-08 20:09:31 +0300

    Рекурсия: 3 из 4 выполнено

--- src/Seminar_8/Recursion.java
+++ src/Seminar_8/Recursion.java
@@ -1,10 +1,15 @@
 package Seminar_8;
 
+import java.beans.Expression;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Recursion {
+    interface Equation{
+        double f(double x);
+    }
+
     public static void allNumber(int n) {
         if (n != 1) allNumber(n - 1);
         System.out.println(n);
@@ -16,9 +21,7 @@ public class Recursion {
         return elemInArr1(arr, elem, index + 1);
     }
 
-    public static <T> boolean elemInArr1(ArrayList<T> arr, T elem) {
-        return elemInArr1(arr, elem, 0);
-    }
+    public static <T> boolean elemInArr1(ArrayList<T> arr, T elem) { return elemInArr1(arr, elem, 0); }
 
     public static boolean elemInArr2(ArrayList<Integer> arr, int elem, int low, int high) {
         int mid = (low + high) / 2;
@@ -29,9 +32,21 @@ public class Recursion {
         return elemInArr2(arr, elem, low, high);
     }
 
-    public static <T> boolean elemInArr2(ArrayList<Integer> arr, int elem) {
-        return elemInArr2(arr, elem, 0, arr.size() - 1);
+    public static <T> boolean elemInArr2(ArrayList<Integer> arr, int elem) { return elemInArr2(arr, elem, 0, arr.size() - 1); }
+
+    public static double equation(Equation functionEq, double a, double b, double eps) {
+        double mid = (b + a) / 2;
+        if (Math.abs(b - a) <= 2 * eps) return a;
+        else if (functionEq.f(mid) * functionEq.f(b) <= 0) a = mid;
+        else b = mid;
+        return equation(functionEq, a, b, eps);
     }
+
+    public static double equation(Equation functionEq, double a, double b) { return equation(functionEq, a, b, 0.001); }
+
+    public static double equation(Equation functionEq, double a) { return equation(functionEq, a, 10, 0.001); }
+
+    public static double equation(Equation functionEq) { return equation(functionEq, 0, 10, 0.001); }
 
     public static void binaryTreeSearch() {
         
@@ -50,6 +65,8 @@ public class Recursion {
         arr.add(49);
 //        System.out.println(elemInArr1(arr, 3));
         arr.sort(Comparator.comparingInt(x -> x));
-        System.out.println(elemInArr2(arr, -1));
+//        System.out.println(elemInArr2(arr, -1));
+        Equation func = (x) -> Math.pow(x, 2) - 89 * x + 21;
+        System.out.printf("%.3f",equation(func));
     }
 }
